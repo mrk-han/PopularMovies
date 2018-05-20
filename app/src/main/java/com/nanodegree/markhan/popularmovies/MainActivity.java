da9cfbf49e9479344ec06a90cf14665ce1e9562f
mrk-han <mark.han@willowtreeapps.com>
2018-05-20 01:01:00 -0400

    implement recyclerview

---
+++ app/src/main/java/com/nanodegree/markhan/popularmovies/MainActivity.java
@@ -0,0 +1,37 @@
+package com.nanodegree.markhan.popularmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.nanodegree.markhan.popularmovies.Model.Movie;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.movie_thumbail_recyclerview) RecyclerView movieRecyclerView;
+
+    ArrayList<Movie> movies;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize movies
+        // TODO: make an async call to fetch the most popular 20 movies from api and put them into ArrayList
+
+        // Create adapter passing in the movie data
+        MovieAdapter adapter = new MovieAdapter(null);
+
+        // Attach the adapter to the recyclerview to populate items
+        movieRecyclerView.setAdapter(adapter);
+        movieRecyclerView.setHasFixedSize(true); // may improve performance depending on size of recyclerview
+        movieRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+    }
+}
---
+++ app/src/main/java/com/nanodegree/markhan/popularmovies/MovieAdapter.java
@@ -0,0 +1,74 @@
+package com.nanodegree.markhan.popularmovies;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.nanodegree.markhan.popularmovies.Model.Movie;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MovieAdapter extends
+        RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+
+    private List<Movie> movies;
+
+    public MovieAdapter(List<Movie> movies) {
+        this.movies = movies;
+    }
+
+    // Inflate the custom layout from XML and return the holder
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Inflate custom layout
+        View movieView = inflater.inflate(R.layout.movie_poster, parent, false);
+
+        // Return a new holder instance
+        return new ViewHolder(movieView);
+    }
+
+    // Populates data into the item through holder
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // Get the data model based on position
+        Movie movie = movies.get(position);
+
+        // Set item views based on your views and data model
+        // TODO: Use Picasso to fetch Image
+
+
+
+    }
+
+    // Returns the total count of movies in the list
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        @BindView(R.id.movie_poster_imageview) ImageView moviePosterIV;
+
+        public ViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, moviePosterIV);
+            view.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+        }
+    }
+}
